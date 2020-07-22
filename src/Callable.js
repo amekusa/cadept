a928941ab2d6149458d86a4312132608f843007f
amekusa <hello@amekusa.com>
2020-07-22 16:11:18 +0900

    Initialize

---
+++ src/Callable.js
@@ -0,0 +1,17 @@
+/**
+ * Extensible Function
+ */
+class Callable extends Function {
+	constructor() {
+		super('...args', 'return this.__self.__call(...args)');
+		this.__self = this.bind(this);
+		return this.__self;
+	}
+	/**
+	 * Runs when the instance is called as function.
+	 * Override to implement
+	 */
+	__call() {}
+}
+
+export default Callable;
