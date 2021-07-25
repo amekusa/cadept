268a7969a2bd20d9cd721dce00954497cc23e84d
amekusa <amekusa@fastmail.com>
2021-07-25 13:59:42 +0900

    + rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+const config = {
+	input: 'src/Task.js',
+	output: {
+		file: './bundle.js',
+		format: 'umd',
+		name: 'cadept',
+		sourcemap: true,
+		validate: true
+	}
+};
+
+export default config;
